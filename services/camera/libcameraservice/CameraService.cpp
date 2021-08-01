9582c9c85755841650c1ec0a35b8540a8a33493f
spezi77 <spezi7713@gmx.net>
2021-08-01 15:21:23 +0200

    Revert "libcameraservice: set property for camera motor"
    This reverts commit 3e65580002f4edbacbbd111cb6a206edd3b05785.

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -103,9 +103,6 @@ using hardware::camera2::utils::ConcurrentCameraIdCombination;
 // Logging support -- this is for debugging only
 // Use "adb shell dumpsys media.camera -v 1" to change it.
 volatile int32_t gLogLevel = 0;
-#ifdef TARGET_MOTORIZED_CAMERA
-time_t motorTimeElapsed = 0;
-#endif
 
 #define LOG1(...) ALOGD_IF(gLogLevel >= 1, __VA_ARGS__);
 #define LOG2(...) ALOGD_IF(gLogLevel >= 2, __VA_ARGS__);
@@ -1646,19 +1643,6 @@ Status CameraService::connectHelper(const sp<CALLBACK>& cameraCb, const String8&
 
     String8 clientName8(clientPackageName);
 
-#ifdef TARGET_MOTORIZED_CAMERA
-    std::string camId = cameraId.string();
-    time_t now = time(0);
-    double dif = difftime (now,motorTimeElapsed);
-    if (camId.compare("1") == 0) {
-        if (dif < 0.5) {
-            usleep (500000);
-        }
-        property_set("sys.camera.motor.direction", "up");
-        motorTimeElapsed = time(0);
-    }
-#endif
-
     int originalClientPid = 0;
 
     ALOGI("CameraService::connect call (PID %d \"%s\", camera ID %s) for HAL version %s and "
@@ -2873,20 +2857,6 @@ CameraService::BasicClient::~BasicClient() {
 
 binder::Status CameraService::BasicClient::disconnect() {
     binder::Status res = Status::ok();
-
-#ifdef TARGET_MOTORIZED_CAMERA
-    std::string camId = mCameraIdStr.string();
-    time_t now = time(0);
-    double dif = difftime (now,motorTimeElapsed);
-    if (camId.compare("1") == 0) {
-        if (dif < 0.5) {
-            usleep (500000);
-        }
-        property_set("sys.camera.motor.direction", "down");
-        motorTimeElapsed = time(0);
-    }
-#endif
-
     if (mDisconnected) {
         return res;
     }
